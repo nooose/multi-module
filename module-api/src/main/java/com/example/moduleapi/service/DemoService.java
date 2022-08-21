8c0e36710fafda0040b6025ce4a5425e58b4115d
nooose <hd15807@gmail.com>
2022-08-21 19:33:48 +0900

    feat: common 모듈에 데모 서비스 추가
    bean 의존성 테스트를 위해 서비스를 추가한다.

--- module-api/src/main/java/com/example/moduleapi/service/DemoService.java
+++ module-api/src/main/java/com/example/moduleapi/service/DemoService.java
@@ -1,13 +1,18 @@
 package com.example.moduleapi.service;
 
 import com.example.modulecommon.enums.CodeEnum;
+import com.example.modulecommon.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class DemoService {
+    private final CommonDemoService commonDemoService;
 
     public String save() {
-        System.out.println(CodeEnum.SUCCESS);
+        System.out.println(CodeEnum.SUCCESS.getCode());
+        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
