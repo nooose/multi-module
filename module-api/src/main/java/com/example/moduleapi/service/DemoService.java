d83a0f2e7915a6d1d9fd41e494f84c6a82fca559
nooose <hd15807@gmail.com>
2022-08-21 21:19:04 +0900

    feat: Repository, Entity 추가

--- module-api/src/main/java/com/example/moduleapi/service/DemoService.java
+++ module-api/src/main/java/com/example/moduleapi/service/DemoService.java
@@ -1,6 +1,7 @@
 package com.example.moduleapi.service;
 
-import com.example.modulecommon.enums.CodeEnum;
+import com.example.modulecommon.domain.Member;
+import com.example.modulecommon.repositories.MemberRepository;
 import com.example.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoService {
     private final CommonDemoService commonDemoService;
+    private final MemberRepository memberRepository;
 
     public String save() {
-        System.out.println(CodeEnum.SUCCESS.getCode());
-        System.out.println(commonDemoService.commonService());
+        memberRepository.save(Member.builder()
+                                    .name(Thread.currentThread().getName())
+                                    .build());
         return "save";
     }
 
     public String find() {
+        int size = memberRepository.findAll().size();
+        System.out.println("DB size : " + size);
         return "find";
     }
 
---
+++ module-common/src/main/java/com/example/modulecommon/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.modulecommon.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
