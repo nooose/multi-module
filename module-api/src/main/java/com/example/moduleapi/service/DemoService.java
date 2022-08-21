b88ccff2ce403155ad9c57f56ebdc62f35fa90e8
nooose <hd15807@gmail.com>
2022-08-21 21:58:07 +0900

    feat: 멀티 Profile 설정

--- module-api/src/main/java/com/example/moduleapi/service/DemoService.java
+++ module-api/src/main/java/com/example/moduleapi/service/DemoService.java
@@ -1,18 +1,25 @@
 package com.example.moduleapi.service;
 
+ import com.example.moduleapi.exception.CustomException;
 import com.example.modulecommon.domain.Member;
+import com.example.modulecommon.enums.CodeEnum;
 import com.example.modulecommon.repositories.MemberRepository;
-import com.example.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class DemoService {
-    private final CommonDemoService commonDemoService;
+
+    @Value("${profile-name}")
+    private String name;
+
     private final MemberRepository memberRepository;
 
     public String save() {
+        System.out.println("name : " + name);
+
         memberRepository.save(Member.builder()
                                     .name(Thread.currentThread().getName())
                                     .build());
@@ -26,6 +33,10 @@ public class DemoService {
     }
 
     public String exception() {
+        if (true) {
+            throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+        }
+
         return "exception";
     }
 }
